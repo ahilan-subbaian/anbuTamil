614393d7e384e6b62656cea7847b0a7af6b72245
Nithi Subbaian <nithmails1@gmail.com>
2026-02-27 19:13:49 +0000

    chore: remove converted static HTML name listings

--- old_html_files/BuildNav.js
+++
@@ -1,9 +0,0 @@
-document.write("<table>");
-
-document.write("<tr><td> <a href='http://www.anbutamil.com'><img src='home.gif' alt='Home'</img></a> </td></tr>");
-document.write("<tr><td> <a href='BoyNamesA.htm'><img src='boynames.gif' alt='Boy Names'</img></a> </td></tr>");
-document.write("<tr><td> <a href='GirlNamesA.htm'><img src='girlnames.gif' alt='Girl Names'</img></a></td></tr>");
-document.write("<tr><td> <a href='Links.htm'><img src='links.gif' alt='Links'</img></a></td></tr>");
-document.write("<tr><td> <a href='ContactUs.htm'><img src='contactus.gif' alt='Contact Us'</img></a></td></tr>");
-
-document.write("</table>");
